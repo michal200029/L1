af25031a4ee85439874f79720e5f3089a9ea5b73
Dawid <dawidn392@gmail.com>
2023-10-09 17:27:23 +0200

    Initial Commit

---
+++ L1/L1App.swift
@@ -0,0 +1,17 @@
+//
+//  L1App.swift
+//  L1
+//
+//  Created by Dawid Nalepa on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct L1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
